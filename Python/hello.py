c1d9bf85393267678d08dd5dd1e7cef408060b63
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-12-20 22:21:33 +0300

    Python

---
+++ Python/hello.py
@@ -0,0 +1,10 @@
+print('Hello, world!')
+
+#VARIABLES
+a = 28 #int
+b = 1.5 #float
+c = "Hello!" #str
+d = True #bool
+e = None #NoneType
+print(a, b, c, d, e)
+
---
+++ Python/name.py
@@ -0,0 +1,5 @@
+name = input('Type your Name:')
+print('Hello, ' + name)
+
+#Fstrings
+print(f"Hello, {name}")
