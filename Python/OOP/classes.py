b3373c112f27ec482caf44a8c6d20f0caa344923
Rem Simiyu <remsimiyu@gmail.com>
2024-12-24 22:29:35 +0300

    Python

--- Python/OOP/classes.py
+++ Python/OOP/classes.py
@@ -1,11 +1,11 @@
-# class Point():
-#     def __init__(self, input1, input2):
-#         self.x = input1
-#         self.y = input2
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
 
-#         p = Point(2, 8)
-#         print(p.x)
-#         print(p.y)
+        p = Point(2, 8)
+        print(p.x)
+        print(p.y)
 
 class Flight():
     def __init__(self, capacity):
