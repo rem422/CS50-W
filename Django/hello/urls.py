91659707665ca4a30703b230e84cab6c0fc076fb
Rem Simiyu <remsimiyu@gmail.com>
2024-12-25 21:59:41 +0300

    Django

--- Django/hello/urls.py
+++ Django/hello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'),
+    path('<str:name>', views.greet, name='greet'),
+    path("rem", views.rem, name='rem'),
+    path("david", views.david, name='david')
 ]
--- Django/hello/views.py
+++ Django/hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def rem(request):
+    return HttpResponse('Hello, Rem!')
+
+def david(request):
+    return HttpResponse('Hello, David!')
+
+def greet(request, name):
+    return render(request,'hello/greet.html', {
+        'name': name.capitalize()
+    })
