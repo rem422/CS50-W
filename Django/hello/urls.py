22a922870b8c19d5686e5c7edafa8120e4011a55
Rem Simiyu <remsimiyu@gmail.com>
2025-01-03 14:23:32 +0300

    Deleted django

--- Django/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('', views.index, name='index'),
-    path('<str:name>', views.greet, name='greet'),
-    path("rem", views.rem, name='rem'),
-    path("david", views.david, name='david')
-]
