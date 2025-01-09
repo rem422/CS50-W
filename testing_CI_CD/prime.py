1fa8f3febf84757ef1ef99abc3d8e03f3d18f526
Rem Simiyu <remsimiyu@gmail.com>
2025-01-09 15:47:19 +0300

    UI

--- testing_CI_CD/prime.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def is_prime(n):
-    if(n < 2):
-        return False
-    for i in range(2, math.sqrt(n)):
-        if n % i == 0:
-            return False
-    return True
--- testing_CI_CD/tests0.py
+++
@@ -1,5 +0,0 @@
-from prime import is_Prime
-
-def test_prime(n, expected):
-    if is_prime(n) != expected:
-        print(f"Error on is_prime({n}), expected {expected}")
